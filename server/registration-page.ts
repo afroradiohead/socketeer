462027cd961c42981e0de9d1ca95e563b37e1003
Christopher Clarke <afroradiohead@gmail.com>
2017-10-20 13:30:34 -0700

    Update registration-page.ts

--- server/registration-page.ts
+++ server/registration-page.ts
@@ -5,8 +5,8 @@ import {IUser} from '../shared/interface/user';
 //...
 const socketeer = new Socketeer(SocketCommand, socket);
 
-socketeer.from('REGISTER_FROMCLIENT').subscribe(request => {
-  //request.email, request.password
+socketeer.from('REGISTER_FROMCLIENT').subscribe(data => {
+  //data.email, data.password
   const user:IUser; //..grab user from database or whatever
  
   socketeer.send('REGISTER_FROMSERVER', { //will throw a typescript compile error if value doesn't match the interface it expects
