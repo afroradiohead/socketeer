0493d3668f3413c64201b907eab7a7051258267c
Christopher Clarke <afroradiohead@gmail.com>
2017-10-20 13:18:48 -0700

    Update and rename example.ts to user.ts

---
+++ server/user.ts
@@ -0,0 +1,16 @@
+import {SocketCommand} from '../shared/sockeeter/user';
+import {Socketeer} from '../shared/socketeer/index';
+import {IUser} from '../shared/interface/user';
+
+//...
+const socketeer = new Socketeer(SocketCommand, socket);
+
+socketeer.from('REGISTER_FROMCLIENT').subscribe(request => {
+  //request.email, request.password
+  const user:IUser; //..grab user from database or whatever
+ 
+  socketeer.send('REGISTER_FROMSERVER', {
+    user: user //will throw a typescript error if not a IUser
+  });
+});
+//...
