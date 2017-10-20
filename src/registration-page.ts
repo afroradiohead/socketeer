6b53d97215efbad5175fa6232b87d93210d1f5ec
Christopher Clarke <afroradiohead@gmail.com>
2017-10-20 13:24:25 -0700

    Update registration-page.ts

--- src/registration-page.ts
+++ src/registration-page.ts
@@ -6,6 +6,8 @@ import {IUser} from '../shared/interface/user';
 const socketeer = new Socketeer(SocketCommand, socket);
 //....
 const user$ = socketeer.from('REGISTER_FROMSERVER').map(request => request.user);
+
+//when form is submitted
 socketeer.send('REGISTER_FROMCLIENT', { //will throw a typescript compile object doesn't match type
   email: "chrissychris@gmail.com",
   password: "youknowdatpassword"
